8bf6934eb81740fa6e6f1bca49e3cdb36781c92c
Elijah Mbugi <mukundielijah@gmail.com>
2022-03-19 13:45:19 +0300

    Call user input values

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
     $("#btn3").click(function(){
+        // Call user inputs
         var largePizza = $("#largepizza").val();
         var mediumPizza = $("#mediumpizza").val();
         var smallPizza = $("#smallpizza").val();
+
+        var cripsyCrust = $("#cripsy").val();
+        var stuffedCrust = $("#stuffed").val();
+        var gluttenCrust = $("#glutten-free").val();
+
+        var berbecueTopping = $("#bbq").val();
+        var pepperoniTopping = $("#pepperoni").val();
+        var hawaiiTopping = $("#hawaii").val();
+
+        var orderNumber = $("#ordernumber").val();
+        console.log(orderNumber);
+
+        // Declare prices
+
+        
 
     })
     
