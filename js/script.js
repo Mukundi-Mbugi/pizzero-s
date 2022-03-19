640887d940dfd08e437afbb313120026389441bb
Elijah Mbugi <mukundielijah@gmail.com>
2022-03-19 13:33:54 +0300

    Get pizza size values

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-// $(document).ready(function(){
-//     alert("fingerlicking good!");
-// })
+$(document).ready(function(){
+    $("#btn3").click(function(){
+        var largePizza = $("#largepizza").val();
+        var mediumPizza = $("#mediumpizza").val();
+        var smallPizza = $("#smallpizza").val();
+
+    })
+    
+})
