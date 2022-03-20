88a04d6c0efbbf2e81b1eefe7bff8d2d294fa053
Elijah Mbugi <mukundielijah@gmail.com>
2022-03-20 10:43:46 +0300

    Add total trice code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
     $("#btn3").click(function(){
         // Call user inputs
-        var largePizza = $("#largepizza").val();
-        var mediumPizza = $("#mediumpizza").val();
-        var smallPizza = $("#smallpizza").val();
-
-        
-
-        var totalLargePizza = {}
+        var pizzaSize = $("#size").val();
+        var pizzaCrust = $("#crust").val();
+        var pizzaTopping = $("#topping").val();
+        var orderNumber = $("#ordernumber").val();
+        // console.log(pizzaSize, pizzaCrust, pizzaTopping);   
+  
+        var totalPrice = (parseInt(pizzaSize) + parseInt(pizzaCrust) + parseInt(pizzaTopping)) * parseInt(orderNumber);
+        console.log(totalPrice);
 
 
 
